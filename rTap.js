645df1e4a5d304fda95b2ce7a35f635c0731195b
Eric Wyne <ecwyne@gmail.com>
2015-02-05 12:38:07 -0800

    testing

--- rTap.js
+++ rTap.js
@@ -1,2 +1,7 @@
-console.log('this is rTap.js');
-console.log('how long does it take to update the repo?');
+jQuery.expr[':'].hasText = function(element, index){
+	if (element.childNotes.length == 1 && element.firstChild.nodeType == 3){
+		return jQuery.trim(element.innerHTML).length > 0;
+	}
+	return false;
+}
+console.log(jQuery(':contains("8-8867"):hasText').get());
