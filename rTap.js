fb6da5da11c1ad175401032b343c9bbce13dd9e5
Leah Thompson <lthompson@gp.local>
2017-01-24 15:20:22 -0800

    New numbers (again)
    Revert reverted changes

--- rTap.js
+++ rTap.js
@@ -1,29 +1,29 @@
 var infusionActions = {
-	'gradepotentialtutoringslocounty.com': 'https://zcu92250.infusionsoft.com/app/form/process/0778eea6b1fc774affd4aa5d56e81c28',
-	'tutoringsantabarbaraarea.com': 'https://zcu92250.infusionsoft.com/app/form/process/6b4f96f0f3c3f35d8a884cb25b1defbd',
-	'tutoringventuracounty.com': 'https://zcu92250.infusionsoft.com/app/form/process/2dcd474ccda47149d3469826d2851c58',
-	'gradepotentialtutoringsandiego.com': 'https://zcu92250.infusionsoft.com/app/form/process/a6162ac29f92252f8c8fdc67f47b7997',
-	'gradepotentialtutoringoc.com': 'https://zcu92250.infusionsoft.com/app/form/process/a4f8da8470a83cdd65f63139f3585ca3',
-	'tutoringsanfernandovalley.com': 'https://zcu92250.infusionsoft.com/app/form/process/92fa0256460fd61581f836bbfda26b65',
-	'tutoringsangabrielvalley.com': 'https://zcu92250.infusionsoft.com/app/form/process/260631a137d189c927601e7b59feebcc',
-	'tutoringlongbeacharea.com': 'https://zcu92250.infusionsoft.com/app/form/process/d6a76088bd4793069a7c8e6b9ce352c6',
-	'losangelestutoringservice.com': 'https://zcu92250.infusionsoft.com/app/form/process/b584e83c34505d83bd6d877e66c4508d',
-	'tutoringinlandempire.com': 'https://zcu92250.infusionsoft.com/app/form/process/69169f8188036abe3e14818d3d9e7f50',
-	'tutoringeastbayarea.com': 'https://zcu92250.infusionsoft.com/app/form/process/031e120b1229664d51461810e79762d6',
-	'tutoringsouthbayarea.com': 'https://zcu92250.infusionsoft.com/app/form/process/bb51ae0c0f8fcaf9128f3736b5bbe15a',
-	'tutoringpeninsula.com': 'https://zcu92250.infusionsoft.com/app/form/process/4c8a106217afc93842d75f8eda9cae76',
-	'sanfranciscotutoringservice.com': 'https://zcu92250.infusionsoft.com/app/form/process/3ac7add41176bf5768cde2bbbdb54f2d',
-	'gradepotentialtutoringsacmetro.com': 'https://zcu92250.infusionsoft.com/app/form/process/3ec0c16bfd5cfe1c6bd871c8b290f25e',
-	'tutoringseattlemetroarea.com': 'https://zcu92250.infusionsoft.com/app/form/process/99fd61701f2f4c471f8cb75fe7f227a2',
-	'tutoringportlandmetroarea.com': 'https://zcu92250.infusionsoft.com/app/form/process/c23da7e66a456509af458e66e46f0ffd',
-	'tutoringvegasmetroarea.com': 'https://zcu92250.infusionsoft.com/app/form/process/a01083fe73c9a230a66adbde2ccc9179',
-	'tutoringphoenixmetroarea.com': 'https://zcu92250.infusionsoft.com/app/form/process/48f65a610ed828e3c13833169773b92a',
-	'tutoringtucsonmetroarea.com': 'https://zcu92250.infusionsoft.com/app/form/process/1466de1f32410c72c00b89e9e55a8b2b'
+  'gradepotentialtutoringslocounty.com': 'https://zcu92250.infusionsoft.com/app/form/process/0778eea6b1fc774affd4aa5d56e81c28',
+  'tutoringsantabarbaraarea.com': 'https://zcu92250.infusionsoft.com/app/form/process/6b4f96f0f3c3f35d8a884cb25b1defbd',
+  'tutoringventuracounty.com': 'https://zcu92250.infusionsoft.com/app/form/process/2dcd474ccda47149d3469826d2851c58',
+  'gradepotentialtutoringsandiego.com': 'https://zcu92250.infusionsoft.com/app/form/process/a6162ac29f92252f8c8fdc67f47b7997',
+  'gradepotentialtutoringoc.com': 'https://zcu92250.infusionsoft.com/app/form/process/a4f8da8470a83cdd65f63139f3585ca3',
+  'tutoringsanfernandovalley.com': 'https://zcu92250.infusionsoft.com/app/form/process/92fa0256460fd61581f836bbfda26b65',
+  'tutoringsangabrielvalley.com': 'https://zcu92250.infusionsoft.com/app/form/process/260631a137d189c927601e7b59feebcc',
+  'tutoringlongbeacharea.com': 'https://zcu92250.infusionsoft.com/app/form/process/d6a76088bd4793069a7c8e6b9ce352c6',
+  'losangelestutoringservice.com': 'https://zcu92250.infusionsoft.com/app/form/process/b584e83c34505d83bd6d877e66c4508d',
+  'tutoringinlandempire.com': 'https://zcu92250.infusionsoft.com/app/form/process/69169f8188036abe3e14818d3d9e7f50',
+  'tutoringeastbayarea.com': 'https://zcu92250.infusionsoft.com/app/form/process/031e120b1229664d51461810e79762d6',
+  'tutoringsouthbayarea.com': 'https://zcu92250.infusionsoft.com/app/form/process/bb51ae0c0f8fcaf9128f3736b5bbe15a',
+  'tutoringpeninsula.com': 'https://zcu92250.infusionsoft.com/app/form/process/4c8a106217afc93842d75f8eda9cae76',
+  'sanfranciscotutoringservice.com': 'https://zcu92250.infusionsoft.com/app/form/process/3ac7add41176bf5768cde2bbbdb54f2d',
+  'gradepotentialtutoringsacmetro.com': 'https://zcu92250.infusionsoft.com/app/form/process/3ec0c16bfd5cfe1c6bd871c8b290f25e',
+  'tutoringseattlemetroarea.com': 'https://zcu92250.infusionsoft.com/app/form/process/99fd61701f2f4c471f8cb75fe7f227a2',
+  'tutoringportlandmetroarea.com': 'https://zcu92250.infusionsoft.com/app/form/process/c23da7e66a456509af458e66e46f0ffd',
+  'tutoringvegasmetroarea.com': 'https://zcu92250.infusionsoft.com/app/form/process/a01083fe73c9a230a66adbde2ccc9179',
+  'tutoringphoenixmetroarea.com': 'https://zcu92250.infusionsoft.com/app/form/process/48f65a610ed828e3c13833169773b92a',
+  'tutoringtucsonmetroarea.com': 'https://zcu92250.infusionsoft.com/app/form/process/1466de1f32410c72c00b89e9e55a8b2b'
 };
 var infusionAction = infusionActions[document.location.hostname.toLowerCase()];
 if (infusionAction){
-	document.getElementById('InfoForm').action = infusionAction;
-	document.getElementById('modalForm').action = infusionAction;
+  document.getElementById('InfoForm').action = infusionAction;
+  document.getElementById('modalForm').action = infusionAction;
 }
 
 var arr = [
@@ -65,12 +65,20 @@ var arr = [
 ['(215) 338-8867', 'rTapNumber226153'],
 ['(610) 828-8867', 'rTapNumber226154'],
 ['(760) 788-8867', 'rTapNumber226725'],
+['(760) 260-3229', 'rTapNumber226725'],
+['(760) 206-8288', 'rTapNumber226725'],
+['(760) 257-5509', 'rTapNumber226725'],
+['(760) 260-9145', 'rTapNumber226725'],
+['(760) 203-5406', 'rTapNumber226725'],
+['(760) 205-3413', 'rTapNumber226725'],
 ['(303) 338-8867', 'rTapNumber226723'],
 ['(720) 858-8867', 'rTapNumber226724'],
 ['(858) 578-8867', 'rTapNumber39661', '6522'],
 ['(619) 363-7261', 'rTapNumber39660', '6522'],
 ['(619) 363-5927', 'rTapNumber39660', '6522'],
+['(619) 492-9412', 'rTapNumber39660', '6522'],
 ['(858) 264-5969', 'rTapNumber39661', '6522'],
+['(858) 345-6597', 'rTapNumber39661', '6522'],
 ['(858) 256-4818', 'rTapNumber39661', '6522'],
 ['(206) 448-8867', 'rTapNumber41700', '7159'],
 ['(253) 548-8867', 'rTapNumber41701', '7159'],
@@ -90,6 +98,20 @@ var arr = [
 ['(425) 689-5301', 'rTapNumber41702', '7159'],
 ['(425) 689-5303', 'rTapNumber41702', '7159'],
 ['(253) 313-1890', 'rTapNumber41701', '7159'],
+['(602) 788-8867', 'rTapNumber41649', '7133'],
+['(623) 878-8867', 'rTapNumber41648', '7133'],
+['(480) 998-8867', 'rTapNumber41647', '7133'],
+['(530) 528-8867', 'rTapNumber41641', '7132'],
+['(916) 448-8867', 'rTapNumber41642', '7132'],
+['(949) 248-8867', 'rTapNumber41713', '7167'],
+['(714) 558-8867', 'rTapNumber41712', '7167'],
+['(909) 338-8867', 'rTapNumber37908', '6091'],
+['(951) 678-8867', 'rTapNumber37909', '6091'],
+['(702) 658-8867', 'rTapNumber37877', '6082'],
+['(805) 968-8867', 'rTapNumber41754', '7197'],
+['(503) 238-8867', 'rTapNumber41697', '7158'],
+['(360) 988-8867', 'rTapNumber41698', '7158'],
+['(520) 458-8867', 'rTapNumber41744', '7191'],
 ]
 
 var cookie = {
@@ -111,36 +133,36 @@ var cookie = {
 }
 
 function makePhoneRegExp(phone){
-	return new RegExp(phone.replace('(', '\\(').replace(')', '\\)').replace(' ', '\\s'));
+  return new RegExp(phone.replace('(', '\\(').replace(')', '\\)').replace(' ', '\\s'));
 }
 
 jQuery.expr[':'].hasText = function(element, index){
-	if (element.childNodes.length == 1 && element.firstChild.nodeType == 3){
-		return jQuery.trim(element.innerHTML).length > 0;
-	}
-	return false;
+  if (element.childNodes.length == 1 && element.firstChild.nodeType == 3){
+    return jQuery.trim(element.innerHTML).length > 0;
+  }
+  return false;
 }
 var adiInit = "19056"
 var selector = arr.map(function (e){return ':contains("' + e[0].substr(8, 6) + '"):hasText'}).join(', ');
 jQuery(selector).each(function (i, e){
-	var nums = arr.filter(function (a){return e.textContent.match(makePhoneRegExp(a[0])) && e.textContent.match(makePhoneRegExp(a[0])).length > 0})
-	nums.forEach(function (num){
-		e.innerHTML = e.innerHTML.replace(makePhoneRegExp(num[0]), '<span class="' + num[1] + '">' + num[0] + '</span>');
+  var nums = arr.filter(function (a){return e.textContent.match(makePhoneRegExp(a[0])) && e.textContent.match(makePhoneRegExp(a[0])).length > 0})
+  nums.forEach(function (num){
+    e.innerHTML = e.innerHTML.replace(makePhoneRegExp(num[0]), '<span class="' + num[1] + '">' + num[0] + '</span>');
     if (num[2]){
       adiInit = num[2];
     }
-	});
+  });
 });
 
 var adiRVO = true;
 var adiFunc = null;
 (function() {
-	var adiSrc = document.createElement("script"); adiSrc.type = "text/javascript";
-	adiSrc.async = true;
-	adiSrc.src = ("https:" == document.location.protocol ? "https://static-ssl" : "http://static-cdn")
-	+ ".responsetap.com/static/scripts/rTapTrack.min.js";
-	var s = document.getElementsByTagName("script")[0];
-	s.parentNode.insertBefore(adiSrc, s);
+  var adiSrc = document.createElement("script"); adiSrc.type = "text/javascript";
+  adiSrc.async = true;
+  adiSrc.src = ("https:" == document.location.protocol ? "https://static-ssl" : "http://static-cdn")
+  + ".responsetap.com/static/scripts/rTapTrack.min.js";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(adiSrc, s);
 })();
 
 function rTapPostReplacement(){
@@ -150,13 +172,13 @@ function rTapPostReplacement(){
       success: function (data){
         console.log('Called!!!', data);
         window.uetq = window.uetq || [];
-        window.uetq.push({ 'ec':'phone call', 'ea':'phone call', 'el':'phone call', 'ev': 1 }); 
+        window.uetq.push({ 'ec':'phone call', 'ea':'phone call', 'el':'phone call', 'ev': 1 });
       },
       timeout: 10*60*1000
     });
     jQuery('[class^=rTapNumber]').bind('touchstart', function(){
       window.uetq = window.uetq || [];
-      window.uetq.push({ 'ec':'phone call', 'ea':'phone call', 'el':'phone call', 'ev': 1 }); 
+      window.uetq.push({ 'ec':'phone call', 'ea':'phone call', 'el':'phone call', 'ev': 1 });
       jQuery.ajax({
         url: 'https://data.gradepotential.com/api/rTap/touchstart/' + cookie.get('adiV'),
         success: function(data){console.log(data)}
